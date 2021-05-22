844172119fac180d3e3ce69955d74dcfaa7db219
kremlevmax <kremlevmax1989@gmail.com>
2021-05-22 15:45:46 -0700

    XML Configuration

---
+++ src/main/java/org/example/ExampleController.java
@@ -0,0 +1,12 @@
+package org.example;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ExampleController {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "helloworld";
+    }
+}
