da711d7985b6295f53850c2b65f167925f07d874
kremlevmax <kremlevmax1989@gmail.com>
2021-06-01 20:31:53 -0700

    JDBC API

--- src/main/java/org/example/config/dao/PetDAO.java
+++ src/main/java/org/example/config/dao/PetDAO.java
@@ -3,6 +3,8 @@ package org.example.config.dao;
 import org.example.config.models.Pet;
 import org.springframework.stereotype.Component;
 
+import java.rmi.ConnectException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,17 +13,45 @@ public class PetDAO {
     private static int PET_COUNT;
     private List<Pet> pets;
 
-    {
-        pets = new ArrayList<>();
-        pets.add(new Pet(++PET_COUNT, "Dog", "Lucky"));
-        pets.add(new Pet(++PET_COUNT, "Dog", "Bad Boy"));
-        pets.add(new Pet(++PET_COUNT, "Cat", "Potato"));
-        pets.add(new Pet(++PET_COUNT, "Mouse", "Jerry"));
-        pets.add(new Pet(++PET_COUNT, "Bearded Dragon", "Abraham"));
+    private static final String URL = "jdbc:postgresql://localhost:5432/pets";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "";
 
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public List<Pet> index() {
+        List<Pet> pets = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String SQLRequest = "SELECT*FROM Pet";
+            ResultSet resultSet = statement.executeQuery(SQLRequest);
+
+            while(resultSet.next()) {
+                Pet pet = new Pet();
+                pet.setName(resultSet.getString("name"));
+                pet.setEmail(resultSet.getString("email"));
+                pet.setAge(resultSet.getInt("age"));
+                pet.setId(resultSet.getInt("id"));
+                pet.setType(resultSet.getString("type"));
+                pets.add(pet);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
         return pets;
     }
 
