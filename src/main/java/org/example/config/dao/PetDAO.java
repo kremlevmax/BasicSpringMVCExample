4a4523dda0cbfb26babd8c45370ac905a6e12467
kremlevmax <kremlevmax1989@gmail.com>
2021-06-03 00:13:36 -0700

    JDBC Template

--- src/main/java/org/example/config/dao/PetDAO.java
+++ src/main/java/org/example/config/dao/PetDAO.java
@@ -3,6 +3,7 @@ package org.example.config.dao;
 import org.example.config.models.Pet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -27,18 +28,22 @@ public class PetDAO {
     }
 
     public Pet showById(int id) {
-    return null;
+        return jdbcTemplate.query("SELECT*FROM Pet WHERE id =?", new Object[]{id},
+                new PetRowMapper()).stream().findAny().orElse(null);
     }
 
     public void save(Pet pet){
-
+        jdbcTemplate.update("INSERT INTO Pet VALUES (?, ?, ?, ?, ?)",
+                ++PET_COUNT, pet.getName(), pet.getAge(), pet.getEmail(), pet.getType());
     }
 
     public void update(int id, Pet pet) {
-
+        jdbcTemplate.update("INSERT INTO Pet VALUES (?, ?, ?, ?) WHERE Id = ?",
+                pet.getName(), pet.getAge(), pet.getEmail(), pet.getType(), id);
     }
 
     public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM Pet WHERE id = ?", id);
     }
 }
 
